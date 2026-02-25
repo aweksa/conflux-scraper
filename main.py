4173a895d9d32856891a7b6632a5f6e73d4d5445
Zukr <zaleksa@protonmail.com>
2026-02-25 19:03:07 +0100

    Added page iteration

--- main.py
+++ main.py
@@ -27,3 +27,20 @@ def page_has_product(html: str) -> bool:
     soup = BeautifulSoup(html, "lxml")
     return bool(soup.select(".product-layout"))
 
+def iterate_pages(url : str, delay : float = 0.5) -> Iterator[Tuple[int, str]]:
+    page = 1
+    while True:
+        url = build_page_url(url, page)
+        html = fetch_page(url)
+        if not html:
+            break
+        
+        if not page_has_product(html):
+            break
+        
+        yield page, html
+        
+        page += 1
+        
+        time.sleep(delay)
+        
